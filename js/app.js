3af90b3a46200fda66a3338450e34c45123f94ec
Justina Mutigliengo <99046568+justinamutigliengo@users.noreply.github.com>
2023-11-23 23:16:30 -0300

    commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+
+const productos = [
+    { id: 1, nombre: "Teclado Logitech", precio: 55.000 },
+    { id: 2, nombre: "Monitor Samsung", precio: 125.000 },
+    { id: 3, nombre: "Parlantes Thonet & Vander", precio: 435.925 },
+    { id: 4, nombre: "Mouse Gamer Logitech", precio: 33.425 },
+    { id: 5, nombre: "Disco duro", precio: 57.120 }
+  ];
+  
+
+
