61b5875e91df50a8e338e91357c8f61ca560d4ba
Justina Mutigliengo <99046568+justinamutigliengo@users.noreply.github.com>
2023-11-29 17:46:02 -0300

    dom

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 const productos = [
-    { id: 1, nombre: "Teclado Logitech", precio: 55.000 },
-    { id: 2, nombre: "Monitor Samsung", precio: 125.000 },
+    { id: 1, nombre: "Teclado Logitech", precio: 55.500 },
+    { id: 2, nombre: "Monitor Samsung", precio: 125.250 },
     { id: 3, nombre: "Parlantes Thonet & Vander", precio: 435.925 },
     { id: 4, nombre: "Mouse Gamer Logitech", precio: 33.425 },
     { id: 5, nombre: "Disco duro", precio: 57.120 }
   ];
-   
+
+for (const producto of productos) {
+  let contenedor = document.createElement("div");
+  contenedor.innerHTML = `<h3>${producto.id}</h3>
+                          <p>${producto.nombre}</p>
+                          <b>$ ${producto.precio}</b>;`
+  document.body.appendChild(contenedor);
+
+}
+
 const carrito = [];
    
 function agregarAlCarrito(productoId) {
